12e15b9352022ee4a547aa119cb4787b04d056e6
Reinhold, Frieder <frieder.reinhold@exxeta.com>
2020-01-31 09:54:42 +0100

    initial commit

---
+++ src/main/java/com/exxeta/correomqtt/plugin/validator/XmlXsdValidatorPlugin.java
@@ -0,0 +1,11 @@
+package com.exxeta.correomqtt.plugin.validator;
+
+import com.exxeta.correomqtt.plugin.manager.PermissionPlugin;
+import org.pf4j.PluginWrapper;
+
+public class XmlXsdValidatorPlugin extends PermissionPlugin {
+
+    public XmlXsdValidatorPlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+}
